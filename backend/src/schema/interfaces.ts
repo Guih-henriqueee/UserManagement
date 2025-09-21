2f225d5ffa3d58690cebf0f44e2510ff94d91435
Guilherme Martins <126841778+Guih-henriqueee@users.noreply.github.com>
2025-09-21 03:10:31 +0000

    feat: new architecture on project
    Create infra with docker and start build backend

---
+++ backend/src/schema/interfaces.ts
@@ -0,0 +1,47 @@
+export interface User {
+    id: string;
+    name: string;
+    cpf: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+    token: string;
+
+}
+
+export interface Agendamento {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    quantity: number;
+    dataEntrega: Date;
+    xml: string;
+    commits: string;
+    status: string;
+    createdAt: Date;
+    updatedAt: Date;
+    userCreated: User;
+    userUpdated: User;
+    fornecedor: Fornecedor;
+    userId: string;
+    userName: string;
+    userEmail: string;
+}
+
+export interface Fornecedor{
+    id: string;
+    name: string;
+    cnpj: string;
+    contact: string
+    agendamentos: string;
+}
+
+export interface  Employee {
+    id: string;
+    name: string;
+    document: string;
+    salary: number;
+    manager: string;
+}
---
+++ backend/src/server.ts
@@ -0,0 +1,92 @@
+import fastify from 'fastify';
+import { fastifyCors } from '@fastify/cors';
+import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
+import { RouteEmployes} from './routes/routes';
+import { RouteAgendamentos } from './routes/agendamentos';
+import { RouteFornecedores } from './routes/fornecedores';
+import { RouteUsers } from './routes/user';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+
+app.register(fastifyCors, {
+    origin: '*',
+});
+
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: 'Agendamentos API',
+            description: 'Backend - Painel de Agendamentos',
+            version: '0.0.2v beta - Last update 03/04/2025',
+            contact: {
+                name: 'Guilherme Martins',
+                url: 'https://github.com/guih-henriqueee',
+                email: 'gmartinsdevelop@gmail.com',
+
+                
+            },
+
+        },
+        components: {
+            securitySchemes: {
+                apiKey: {
+                    type: 'apiKey',
+                    name: 'apiKey',
+                    in: 'header',
+                },
+            },
+        },
+        security: [{ apiKey: [] }],
+
+    },
+    transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false
+    },
+    uiHooks: {
+      onRequest: function (_request, _reply, next) {
+        next();
+      },
+      preHandler: function (_request, _reply, next) {
+        next();
+      }
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true
+  
+});
+
+app.register(RouteAgendamentos);
+app.register(RouteUsers);
+app.register(RouteEmployes);
+app.register(RouteFornecedores);
+
+
+app.get('/', () => {
+    return { hello: 'world' };
+});
+
+
+app.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+});
